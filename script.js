a16f383e1228207f5bf1f3f9da8af51995bbf679
Jacob Martinez <176696638+jomartinez26@users.noreply.github.com>
2025-03-16 09:04:36 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let timerInterval;
+let popupInterval;
 let isRunning = false;
 let isPaused = false;
 let totalTimeInSeconds;
@@ -55,7 +56,7 @@ function generateRandomPopups() {
     popupInterval = setInterval(createPopupImage, 6000); 
 }
 
-generateRandomPopups();
+
 
 function startTimer() {
   clearInterval(timerInterval); 
@@ -83,6 +84,8 @@ function startTimer() {
   document.getElementById('pauseButton').disabled = false;
 
   timerInterval = setInterval(updateTimer, 1000);
+    clearInterval(popupInterval);
+    generateRandomPopups();
   watchCheckInterval = setInterval(askStillWatching, 30000);
 }
 
